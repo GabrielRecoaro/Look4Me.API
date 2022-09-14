755767bd4e0b8abb08421ee0ffd471017ec446d7
Guilherme <bastosguilherme12@gmail.com>
2022-09-14 08:39:49 -0300

    Adicionando widget, tela de splash e tela descricao

---
+++ app/src/main/java/com/example/look4me/Descricao.java
@@ -0,0 +1,14 @@
+package com.example.look4me;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Descricao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_descricao);
+    }
+}
